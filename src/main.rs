dfae5f58a19e1908c334f9fc9f320ba219f6e28b
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2024-05-16 08:36:40 +1000

    rm

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,5 @@ use jabara::prelude::*;
 fn main() {
 	tracing_subscriber::fmt::init();
 
-	// let expr = BinaryAddition {
-	// 	lhs: 1.0,
-	// 	rhs: 2.0,
-	// };
-
-	// let variable = VariableID::new("x");
+	
 }
